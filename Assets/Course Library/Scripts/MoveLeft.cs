53570bc00bd5db20825ea47f9c2f3bbeb0962499
EricTran000 <89489669+EricTran000@users.noreply.github.com>
2022-04-26 16:46:50 -0500

    Completed tutorial 3.2

--- Assets/Course Library/Scripts/MoveLeft.cs
+++ Assets/Course Library/Scripts/MoveLeft.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float speed = 30;
+    private PlayerController playerControllerScript;
+    private float leftBound = -15;
 
-    // Start is called before the first frame update
+    // Gets PlayerController Script from the player
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
-    // Update is called once per frame
+    // Update is called once
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        //Moves obstacles left if the game is still continuing
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        
+        //Deletes obstacles out of bounds
+        if(transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Course Library/Scripts/PlayerController.cs
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
 
     public bool isOnGround = true;
+    public bool gameOver = false;
     public float jumpForce = 10;
     public float gravityModifier;
     // Start is called before the first frame update
@@ -28,7 +29,15 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+        }
     }
 
 }
